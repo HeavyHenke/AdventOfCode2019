0b7541fd6b5ce23df54a9514f31d81d6692e2443
HeavyHenke <henrik@hellenborg.com>
2019-12-01 10:04:56 +0100

    Day1

---
+++ Day1.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    class Day1
+    {
+        public string CalcA()
+        {
+            var fuels = File.ReadAllLines("Day1Data.txt")
+                .Select(int.Parse)
+                .Select(f => f / 3)
+                .Select(f => f - 2)
+                .Sum();
+
+
+            return fuels.ToString();
+        }
+
+        public string CalcB()
+        {
+            var fuels = File.ReadAllLines("Day1Data.txt")
+                .Select(int.Parse)
+                .Select(GetTotalFuel)
+                .Sum();
+
+
+            return fuels.ToString();
+        }
+
+        private int GetTotalFuel(int moduleMass)
+        {
+            int total = 0;
+            int mass = moduleMass;
+            int delta;
+
+            do
+            {
+                delta = mass / 3 - 2;
+                if (delta < 0)
+                    delta = 0;
+                total += delta;
+                mass = delta;
+
+            } while (delta > 0);
+
+            return total;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            DateTime start = DateTime.Now;
+            string result = new Day1().CalcB();
+            DateTime stop = DateTime.Now;
+
+            Console.WriteLine("It took " + (stop - start).TotalSeconds);
+
+            Clipboard.SetText(result);
+            Console.WriteLine(result);
+        }
+    }
+}
